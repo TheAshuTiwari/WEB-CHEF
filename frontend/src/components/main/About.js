c050d4aea61eb9bc24cf12985ae857ea2de15f72
TheAshuTiwari <astoshtiwarisln@gmail.com>
2023-05-30 13:46:53 +0530

    ..

--- frontend/src/components/main/About.js
+++ frontend/src/components/main/About.js
@@ -2,7 +2,54 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <>
+  <title>About Us</title>
+  <style
+    dangerouslySetInnerHTML={{
+      __html:
+        "\n    body {\n      font-family: Arial, sans-serif;\n      margin: 0;\n      padding: 0;\n    }\n\n    header {\n      background-color: #f2f2f2;\n      padding: 20px;\n    }\n\n    h1 {\n      color: #333;\n      margin: 0;\n    }\n\n    nav {\n      background-color: #333;\n    }\n\n    nav ul {\n      list-style-type: none;\n      margin: 0;\n      padding: 0;\n    }\n\n    nav ul li {\n      display: inline;\n    }\n\n    nav ul li a {\n      color: #fff;\n      display: inline-block;\n      padding: 10px 20px;\n      text-decoration: none;\n    }\n\n    nav ul li a.active {\n      background-color: #555;\n    }\n\n    section {\n      padding: 20px;\n    }\n\n    h2 {\n      color: #333;\n    }\n\n    footer {\n      background-color: #f2f2f2;\n      padding: 20px;\n      text-align: center;\n      font-size: 12px;\n    }\n  "
+    }}
+  />
+  <header>
+    <h1>About Us</h1>
+  </header>
+
+  <section>
+    <h2>Our Company</h2>
+    <p>
+      Welcome to our company! We are a leading provider of innovative solutions
+      in the industry. With a team of dedicated professionals, we strive to
+      deliver exceptional products and services to our clients.
+    </p>
+    <h2>Our Mission</h2>
+    <p>
+      Our mission is to exceed customer expectations by providing high-quality
+      products and exceptional customer service. We are committed to
+      continuously improving our offerings and staying at the forefront of
+      technology.
+    </p>
+    <h2>Our Team</h2>
+    <p>
+      We have a talented and diverse team of professionals who bring their
+      expertise to every project. Our team is passionate about what they do and
+      works together to achieve our goals.
+    </p>
+    <h2>Why Choose Us</h2>
+    <ul>
+      <li>Experienced professionals</li>
+      <li>Quality products</li>
+      <li>Exceptional customer service</li>
+      <li>Innovative solutions</li>
+      <li>Customer satisfaction</li>
+    </ul>
+  </section>
+  <footer>
+    <p>© 2023 Your Company. All rights reserved.</p>
+  </footer>
+</>
+
+    </div>
   )
 }
 
--- frontend/src/components/main/Contact.js
+++ frontend/src/components/main/Contact.js
@@ -2,7 +2,80 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+        <div  style={{backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/img/contact-us BG.jpg')`,backgroundSize: 'cover',backgroundPosition:'bottom' }}>
+
+<div className="container ">
+  {/* <div className="row"> */}
+
+    {/* <div className="col-md-6 my-5 ">
+      <img src="/img/contact us pic.jpg" alt="contact us photo" className='w-100 img-thumbnail'/>
+    </div> */}
+    {/* <div className="col-md-6 my-5 "> */}
+      <div className="card my-5 mx-auto  p-2" style={{ height: '75vh' ,width:'55vh' }}>
+        <div className="card-body">
+          <h1 className="card-title text-center text-black mb-4">Contact us</h1>
+          <form>
+            {/* Name input */}
+            <div className=" mb-4">
+              <label className="form-label" htmlFor="form4Example1">
+               {/* Name */}
+              </label>
+              <input type="text" id="form4Example1" className="form-control" 
+              placeholder="Name"/>
+            </div>
+            {/* Email input */}
+            <div className=" mb-4">
+              <label className="form-label" htmlFor="form4Example2">
+                {/* Email address */}
+              </label>
+              <input type="email" id="form4Example2" className="form-control"
+              placeholder="Email Address"/>
+            </div>
+            {/* Message input */}
+            <div className=" mb-4">
+              <label className="form-label" htmlFor="form4Example3">
+                {/* Message */}
+              </label>
+              <textarea
+                className="form-control"
+                id="form4Example3"
+                rows={4}
+                defaultValue={""}
+                placeholder='Message' />
+            </div>
+            {/* Checkbox */}
+            <div className="form-check d-flex justify-content-center mb-4">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                defaultValue=""
+                id="form4Example4"
+                defaultChecked=""
+              />
+              <label className="form-check-label text-black" htmlFor="form4Example4">
+                Send me a copy of this message
+              </label>
+            </div>
+            {/* Submit button */}
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary mb-4">
+                Send
+              </button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+  </div>
+    </div>
   )
 }
 
