0ba3d0e248ff72023e57bd2978e6f87258224e33
m-triple-m <triplem656@gmail.com>
2023-06-10 12:44:19 +0530

    ..

--- frontend/src/components/main/Signup.js
+++ frontend/src/components/main/Signup.js
@@ -15,7 +15,9 @@ const Signup = () => {
         'Content-Type': 'application/json'
       }
     })
+    
     console.log(res.status);
+
     if (res.status === 200) {
       Swal.fire({
         icon: 'success',
